e9c80d0450652b9be8ac132f41ee10649df58b2c
Bryn <brynheath1@gmail.com>
2020-11-16 22:52:02 +0000

    init setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  mainContainer: {
+    margin: 'auto',
+    height: '60vh',
+    width: '60vw',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    transform: `translate(${0}, ${25}%)`,
+    borderRadius: 50,
+    borderWidth: 6,
+    borderColor: '#042c34',
+    borderStyle: 'solid',
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class={classes.mainContainer}>
+      <div></div>
+      {'setup'}
+      <div></div>
     </div>
   );
-}
+};
 
 export default App;
